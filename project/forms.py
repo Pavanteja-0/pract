0e03daf18083c7e819efa73525d0029b6a60d4ef
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-11-08 22:40:21 +0530

    Create forms.py

---
+++ project/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import Form
+from wtforms import TextField, TextAreaField, SubmitField
+from wtforms import validators, ValidationError
+from wtforms.validators import Email
+class ContactForm(Form):
+  name = TextField("Name",  [validators.Required("Please enter your name.")])
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  subject = TextField("Subject",  [validators.Required("Please enter a Subject title for us to understand.")])
+  message = TextAreaField("Message",  [validators.Required("Please enter the complete message.")])
+  submit = SubmitField("Send")
