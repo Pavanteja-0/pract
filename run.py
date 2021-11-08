eda5dd76691f5fc0f213ad4fcce410603447d22c
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-11-08 23:28:36 +0530

    Update run.py

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def create_app():
     app.config["MAIL_PORT"] = 465
     app.config["MAIL_USE_SSL"] = True
     app.config["MAIL_USERNAME"] = 'pavanteja14@gmail.com'
-    app.config["MAIL_PASSWORD"] = 'abyqaklzhesxzrmi'
+    app.config["MAIL_PASSWORD"] = 'fuckme@10PM'
     mail.init_app(app)
     
     
