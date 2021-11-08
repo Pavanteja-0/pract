0f3dc4494aa5b6aade96cba33e419c0cba956a33
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-11-08 22:50:20 +0530

    init_mail

--- project/main.py
+++ project/main.py
@@ -1,9 +1,27 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for, Flask, session, flash
 from flask import app
 import json
+from project.forms import ContactForm
+from flask_mail import Message, Mail
+from run import mail
 
 main = Blueprint('main', __name__)
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    
+    form = ContactForm()
+    if request.method == 'POST':
+      if form.validate() == False:
+        flash('All fields are required.')
+        return render_template("index.html", form=form)
+      else:
+        msg = Message(form.subject.data, sender='contact@example.com', recipients=['pavanteja14@gmail.com'])
+        msg.body = """
+        From: %s &lt;%s&gt;
+        %s
+        """ % (form.name.data, form.email.data, form.message.data)
+        mail.send(msg)
+        return render_template('index.html', success=True)
+    elif request.method == 'GET':
+      return render_template("index.html", form=form)
