5df242d5eee65ad80239bd6c1a8ed758855765fb
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-11-08 23:04:54 +0530

    StringField

--- project/forms.py
+++ project/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import Form
-from wtforms.fields import TextField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms import validators, ValidationError
 from wtforms.validators import Email
 class ContactForm(Form):
-  name = TextField("Name",  [validators.Required("Please enter your name.")])
-  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
-  subject = TextField("Subject",  [validators.Required("Please enter a Subject title for us to understand.")])
+  name = StringField("Name",  [validators.Required("Please enter your name.")])
+  email = StringField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  subject = StringField("Subject",  [validators.Required("Please enter a Subject title for us to understand.")])
   message = TextAreaField("Message",  [validators.Required("Please enter the complete message.")])
   submit = SubmitField("Send")
