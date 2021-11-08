28834909f1f90ad2d87bccdd880ea7947393eb82
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-11-08 23:25:22 +0530

    Update run.py

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ mail = Mail()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'devkey'
     app.config["MAIL_SERVER"] = "smtp.gmail.com"
     app.config["MAIL_PORT"] = 465
     app.config["MAIL_USE_SSL"] = True
