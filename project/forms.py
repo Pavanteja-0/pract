3dd11caabcaf394577a4f3a0e1a9175ed0505fc5
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-11-08 23:08:13 +0530

    Update forms.py

--- project/forms.py
+++ project/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms import validators, ValidationError
 from wtforms.validators import Email
 class ContactForm(Form):
-  name = StringField("Name",  [validators.Required("Please enter your name.")])
-  email = StringField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
-  subject = StringField("Subject",  [validators.Required("Please enter a Subject title for us to understand.")])
-  message = TextAreaField("Message",  [validators.Required("Please enter the complete message.")])
+  name = StringField("Name",  [validators.DataRequired("Please enter your name.")])
+  email = StringField("Email",  [validators.DataRequired("Please enter your email address."), validators.Email("Please enter your email address.")])
+  subject = StringField("Subject",  [validators.DataRequired("Please enter a Subject title for us to understand.")])
+  message = TextAreaField("Message",  [validators.DataRequired("Please enter the complete message.")])
   submit = SubmitField("Send")
